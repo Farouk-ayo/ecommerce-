84d5f0654166628d008eef51ef219dd79808a92c
frkayo <mustaphafarouk41@gmail.com>
2023-01-03 15:43:29 +0100

    contact done

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -1,12 +1,41 @@
 import logo from "../assets/logo.png";
 import arrowDown from "../assets/arrowDown.png";
+import smallArrowDown from "../assets/smallArrowDown.png";
 import person from "../assets/person.png";
 import cart from "../assets/cart.png";
+import call from "../assets/call.png";
+import mail from "../assets/mail.png";
 import classes from "./Header.module.css";
 
 const Header = () => {
   return (
     <section className={classes.headerSection}>
+      <div className={classes.contactDetails}>
+        <div className={classes.contact}>
+          <div className={classes.call}>
+            <img src={call} alt="" />
+            <a href={+2348073946451}>+2348073946451</a>
+          </div>
+          <div className={classes.mail}>
+            <img src={mail} alt="" />
+            <a href={+2348073946451}>Mustaphafarouk41@gmail.com</a>
+          </div>
+        </div>
+        <ul className={classes.details}>
+          <li>
+            <p>Theme FAQ's</p>
+            <p>Need Help?</p>
+            <div>
+              <p>EN</p> <img className={classes.rotateDown} src={smallArrowDown} alt="" />
+            </div>
+            <div>
+              <p>USD </p>
+              <img className={classes.rotateDown} src={smallArrowDown} alt="" />
+            </div>
+          </li>
+        </ul>
+      </div>
+
       <div className={classes.header}>
         <img src={logo} alt="" />
         <input type="search" name="" id="" />
