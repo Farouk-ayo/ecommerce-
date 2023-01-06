2549f8f67477699711f8f1f054bcec74c71a5df7
frkayo <mustaphafarouk41@gmail.com>
2023-01-06 21:26:54 +0100

    about to create the main page"

--- src/UI/ui.js
+++ src/UI/ui.js
@@ -2,12 +2,14 @@ import classes from "./ui.module.css";
 import Header from "./Header";
 import Body from "./Body";
 import React from "react";
+import Main from "./Main";
 
 const Ui = () => {
   return (
     <React.Fragment>
       <Header />
       <Body />
+      <Main />
     </React.Fragment>
   );
 };
