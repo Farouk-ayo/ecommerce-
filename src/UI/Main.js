b8742113c6c0868ba07130402bd92631e52841e3
frkayo <mustaphafarouk41@gmail.com>
2023-01-07 10:52:31 +0100

    about to create the main page"

--- src/UI/Main.js
+++ src/UI/Main.js
@@ -1,9 +1,45 @@
-const Main =()=>{
+const Main = () => {
+  return (
+    <main>
+        <section>
+        <div>
+        <h3>lifestyle collection</h3>
+        <h1>men</h1>
+        <h2>sale up to 30% off</h2>
+        <p>Get Free Shipping on orders over $99.00</p>
+        <button>Shop Now</button>
 
+      </div>
+      <img src={} alt="" />
 
+      <input type="checkbox" name="" id="" />
+      <input type="checkbox" name="" id="" />
+        </section>
+      <section>
+        <div>
+            <span>
+            <h3>new arrivals</h3>
+            <h1>summer <br /> sale 20% off</h1>
+            </span>
+           <a href="">Shop now </a>
 
-    return <div>main</div>
-    
-}
+        </div>
+        <img src="" alt="" />
+      </section>
+      <section>
+        <div>
+            <span>
+            <h3>gamming 4K</h3>
+            <h1>Desktops &<br /> Laptops</h1>
+            </span>
+           <a href="">Shop now </a>
 
-export default Main
+        </div>
+        <img src="" alt="" />
+      </section>
+      
+    </main>
+  );
+};
+
+export default Main;
