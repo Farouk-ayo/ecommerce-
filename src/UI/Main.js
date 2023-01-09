816a80d2a47976fb931315aa56d1e17052282099
frkayo <mustaphafarouk41@gmail.com>
2023-01-09 21:55:17 +0100

    added image"

--- src/UI/Main.js
+++ src/UI/Main.js
@@ -1,43 +1,46 @@
+import mainImage from "../assets/w11.png";
+import classes from "./Main.module.css";
+
 const Main = () => {
   return (
-    <main>
-        <section>
-        <div>
-        <h3>lifestyle collection</h3>
-        <h1>men</h1>
-        <h2>sale up to 30% off</h2>
-        <p>Get Free Shipping on orders over $99.00</p>
-        <button>Shop Now</button>
-
-      </div>
-      <img src={} alt="" />
-
-      <input type="checkbox" name="" id="" />
-      <input type="checkbox" name="" id="" />
-        </section>
+    <main className={classes.main}>
       <section>
         <div>
-            <span>
+          <h3>lifestyle collection</h3>
+          <h1>men</h1>
+          <h2>sale up to 30% off</h2>
+          <p>Get Free Shipping on orders over $99.00</p>
+          <button>Shop Now</button>
+        </div>
+        <img src={mainImage} alt="" />
+
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+      </section>
+      <section>
+        <div>
+          <span>
             <h3>new arrivals</h3>
-            <h1>summer <br /> sale 20% off</h1>
-            </span>
-           <a href="">Shop now </a>
-
+            <h1>
+              summer <br /> sale 20% off
+            </h1>
+          </span>
+          <link rel="stylesheet" href="" />
         </div>
         <img src="" alt="" />
       </section>
       <section>
         <div>
-            <span>
+          <span>
             <h3>gamming 4K</h3>
-            <h1>Desktops &<br /> Laptops</h1>
-            </span>
-           <a href="">Shop now </a>
-
+            <h1>
+              Desktops &<br /> Laptops
+            </h1>
+          </span>
+          <link rel="stylesheet" href="" />
         </div>
         <img src="" alt="" />
       </section>
-      
     </main>
   );
 };
