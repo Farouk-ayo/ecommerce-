e8039f1eea5d5a027d5d09b6d4b638378ce8cb41
frkayo <mustaphafarouk41@gmail.com>
2023-01-22 15:35:11 +0100

    created a store for flashdeals

--- src/UI/Body/flashdeals/Flashsales.js
+++ src/UI/Body/flashdeals/Flashsales.js
@@ -1,6 +1,15 @@
 import { IoFlash } from "react-icons/io5";
 import classes from "./Flashsale.module.css";
 
+import SlidesFlashStore from "./SlidesFlashStore";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/a11y";
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 const Flashsale = () => {
   return (
     <section className={classes.flashsale}>
@@ -8,7 +17,21 @@ const Flashsale = () => {
         <IoFlash color="#f55353" size={30} />
         <h2>Flash Sales</h2>
       </div>
-      <div className={classes.flashcontainer}></div>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        slidesPerView={4}
+        pagination={{
+            clickable:true
+        }}
+      >
+        {SlidesFlashStore.map((value) => {
+          return (
+            <SwiperSlide>
+              <div className={classes.flashcontainer}></div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
---
+++ src/UI/Body/flashdeals/SlidesFlashStore.js
@@ -0,0 +1,77 @@
+import shoeFlash from "../../../assets/shoeFlash.png";
+import iphoneFlash from "../../../assets/iphone.png";
+import Necklace from "../../../assets/necklace.png";
+import smartwatchFlash from "../../../assets/smartwatch.png";
+import dellFlash from "../../../assets/dell.png";
+import laptopBagFlash from "../../../assets/laptopbag.png";
+import hoodieFlash from "../../../assets/hoodie.png";
+import cameraFlash from "../../../assets/camera.png";
+
+const SlidesFlashStore = [
+  {
+    key: 1,
+    product: "Shoe",
+    productImage: shoeFlash,
+    percentOff: "50% off",
+    ratings: "5",
+    price: "$20.00",
+  },
+  {
+    key: 2,
+    product: "Iphone",
+    productImage: iphoneFlash,
+    percentOff: "30% off",
+    ratings: "5",
+    price: "$320.00",
+  },
+  {
+    key: 3,
+    product: "Necklace",
+    productImage: watchFlash,
+    percentOff: "10% off",
+    ratings: "4",
+    price: "$50.00",
+  },
+  {
+    key: 4,
+    product: "SmartWatch",
+    productImage: smartwatchFlash,
+    percentOff: "50% off",
+    ratings: "5",
+    price: "$20.00",
+  },
+  {
+    key: 5,
+    product: "Laptop Dell-XPS",
+    productImage: dellFlash,
+    percentOff: "25% off",
+    ratings: "5",
+    price: "$520.00",
+  },
+  {
+    key: 6,
+    product: "Laptop Bag",
+    productImage: laptopBagFlash,
+    percentOff: "5% off",
+    ratings: "4",
+    price: "$20.00",
+  },
+  {
+    key: 7,
+    product: "Hoodie",
+    productImage: hoodieFlash,
+    percentOff: "15% off",
+    ratings: "3",
+    price: "$30.00",
+  },
+  {
+    key: 8,
+    product: "Camera",
+    productImage: cameraFlash,
+    percentOff: "10% off",
+    ratings: "5",
+    price: "$120.00",
+  },
+];
+
+export default SlidesFlashStore;
