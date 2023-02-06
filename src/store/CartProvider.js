001c04017787ab1fffd9c431b61bfc917a62ee13
frkayo <mustaphafarouk41@gmail.com>
2023-02-06 15:45:56 +0100

    a prgress on useReducers, getting familiarize with it

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import CartCtx from "./Cartctx";
 
 const defaultState = {
   items: [],
@@ -6,13 +7,35 @@ const defaultState = {
 };
 
 const cartReducer = (state, action) => {
-  console.log(state, action);
+  if ((action.type = "ADD")) {
+    const totalAmount = state.totalAmount + action.price;
+  }
 };
 
 const CartProvider = () => {
   const [cartState, dispatchState] = useReducer(cartReducer, defaultState);
 
-  const addCartItems = () => {
-    dispatchState();
+  const addCartItems = (price) => {
+    dispatchState({ type: "ADD", price: price });
   };
+
+  const removeCartItems = (id) => {
+    dispatchState({ type: "REMOVE", id: id });
+  };
+
+  const clearCartItems = () => {
+    dispatchState({ type: "CLEAR" });
+  };
+
+  const cartCtx = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItems: addCartItems,
+    removeItems: removeCartItems,
+    clearItems: clearCartItems,
+  };
+
+  return <CartCtx.Provider value={cartCtx}></CartCtx.Provider>;
 };
+
+export default CartProvider;
