ad1c66a1d3b5e16d7eb61bb12613baa3c42392c4
frkayo <mustaphafarouk41@gmail.com>
2023-02-12 17:00:48 +0100

    tryna fix the bug

--- src/UI/Body/Gender/Women/WomenSidebar.js
+++ src/UI/Body/Gender/Women/WomenSidebar.js
@@ -11,11 +11,7 @@ const WomenSidebar = () => {
       <div className={classes.sec}>
         {Store.women.sections.map((eachSec) => {
           return (
-            <p
-              //  key={eachSec.length}
-              value={eachSec}
-              className={classes.eachSec}
-            >
+            <p key={eachSec.length} value={eachSec} className={classes.eachSec}>
               {eachSec}
             </p>
           );
