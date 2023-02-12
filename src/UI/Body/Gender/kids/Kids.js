d142cfd039b8b6aca06ff46bf30786d66492b521
frkayo <mustaphafarouk41@gmail.com>
2023-02-12 17:27:07 +0100

    tryna fix the bug

--- src/UI/Body/Gender/kids/Kids.js
+++ src/UI/Body/Gender/kids/Kids.js
@@ -1,12 +1,12 @@
 import KidSidebar from "./KidSidebar";
 import classes from "../Store.module.css";
-// import KidsProducts from "./KidsProducts";
+import KidsProducts from "./KidsProducts";
 
 const Kids = (props) => {
   return (
     <section className={classes.section} id="kids">
       <KidSidebar />
-      {/* <KidsProducts /> */}
+      <KidsProducts />
     </section>
   );
 };
