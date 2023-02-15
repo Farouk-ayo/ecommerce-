24f031e719184620d3e0d39cac75e4353bb99142
frkayo <mustaphafarouk41@gmail.com>
2023-02-15 15:29:48 +0100

    routing cart page and payment gateway

--- src/page/CartPage.js
+++ src/page/CartPage.js
@@ -1,7 +1,39 @@
+import { useContext } from "react";
+import { RxCross2 } from "react-icons/rx";
+import CartCtx from "../store/Cartctx";
+import classes from "./Page.module.css";
+import { Link } from "react-router-dom";
+
 const CartPage = () => {
+  const cartCtx = useContext(CartCtx);
   return (
     <section>
-      <div></div>
+      <div>
+        <Link to="./cart"></Link>
+      </div>
+      {cartCtx.items.map((item) => {
+        return (
+          <section className={classes.cartInfo}>
+            <div className={classes.eachInfo}>
+              <div className={classes.btnDiv}>
+                <button className={classes.firstBtn}>+</button>
+                <p>{item.quantity}</p>
+                <button className={classes.secondBtn}>-</button>
+              </div>
+              <img src={item.productImage} alt="" />
+              <div className={classes.details}>
+                <h5>{item.productName}</h5>
+                <p>
+                  <span>${item.price}.00</span>x<span>{item.quantity}</span>
+                </p>
+                <h2>${item.totalAmount}.00</h2>
+              </div>
+              <RxCross2 />
+            </div>
+            <hr />
+          </section>
+        );
+      })}
     </section>
   );
 };
