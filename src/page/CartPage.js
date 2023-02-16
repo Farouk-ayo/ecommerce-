e025f207cc8ff85de87202b4dd5c028104683ff7
frkayo <mustaphafarouk41@gmail.com>
2023-02-16 22:25:16 +0100

    done with cart page

--- src/page/CartPage.js
+++ src/page/CartPage.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 const CartPage = () => {
   const cartCtx = useContext(CartCtx);
   return (
-    <section>
+    <section className={classes.CartPage}>
       <div className={classes.links}>
         <Link to="./cart">1. Cart</Link>
         <hr />
@@ -17,33 +17,44 @@ const CartPage = () => {
         <hr />
         <Link to="./review">4. Review</Link>
       </div>
-      {cartCtx.items.map((item) => {
-        return (
-          <section className={classes.cartInfo}>
-            <div>
-              <img src={item.productImage} alt="" />
-              <div className={classes.eachInfo}>
-                <div className={classes.details}>
-                  <h5>{item.productName}</h5>
-                  <span>
-                    <p>
-                      <span>${item.price}.00</span>x<span>{item.quantity}</span>
-                    </p>
-                    <h2>${item.totalAmount}.00</h2>
-                  </span>
+      <section className={classes.cart}>
+        <div>
+          {cartCtx.items.map((item) => {
+            return (
+              <section className={classes.cartInfo}>
+                <div>
+                  <img src={item.productImage} alt="" />
+                  <div className={classes.eachInfo}>
+                    <div className={classes.details}>
+                      <h5>{item.productName}</h5>
+                      <span>
+                        <p>
+                          <span>${item.price}.00</span>x
+                          <span>{item.quantity}</span>
+                        </p>
+                        <h2>${item.totalAmount}.00</h2>
+                      </span>
+                    </div>
+                    <div className={classes.btnDiv}>
+                      <button className={classes.secondBtn}>-</button>
+                      <p>{item.quantity}</p>
+                      <button className={classes.firstBtn}>+</button>
+                    </div>
+                  </div>
                 </div>
-                <div className={classes.btnDiv}>
-                  <button className={classes.secondBtn}>-</button>
-                  <p>{item.quantity}</p>
-                  <button className={classes.firstBtn}>+</button>
-                </div>
-              </div>
-            </div>
+                <RxCross2 className="RxCross2" />
+              </section>
+            );
+          })}
+        </div>
 
-            <RxCross2  className="RxCross2"/>
-          </section>
-        );
-      })}
+        <section className={classes.totalAmount}>
+          <h3>
+            <span>Total:</span>
+            <span>${cartCtx.totalAmount}</span>
+          </h3>
+        </section>
+      </section>
     </section>
   );
 };
